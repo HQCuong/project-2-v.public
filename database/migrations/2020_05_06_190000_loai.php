039f9cf81921179d3c2b8a5dda4f72f3fe8f65e2
anhtrai <34875016+anhtrai@users.noreply.github.com>
2020-06-12 17:33:32 +0700

    version 0.0.8
    version 0.0.8

--- database/migrations/2020_05_06_190000_loai.php
+++ database/migrations/2020_05_06_190000_loai.php
@@ -12,17 +12,18 @@ class Loai extends Migration {
 	 * @return void
 	 */
 	private $name = 'loai';
-	private $ten_loai = ['case', 'Màn Hình', 'Chuột', 'Bàn Phím'];
-	private $mo_ta = [];
+	private $ten_loai = ['case', 'ma_hinh', 'chuot', 'ban_phim'];
+	private $mo_ta = ['case', 'Màn Hình', 'Chuột', 'Bàn Phím'];
 	public function up() {
 		Schema::create($this->name, function (Blueprint $table) {
 			$table->increments('ma_loai');
-			$table->string('ten_loai', 100);
+			$table->string('ten_loai', 100)->unique();
 			$table->text('mo_ta')->nullable();;
 		});
 		for ($i = 0; $i < count($this->ten_loai); $i++) {
 			DB::table($this->name)->insert(array(
 				'ten_loai' => $this->ten_loai[$i],
+				'mo_ta' => $this->mo_ta[$i],
 			));
 		}
 	}
