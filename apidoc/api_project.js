1fba3829ae27801e790dc84a64a02a22e869a681
anhtrai <34875016+anhtrai@users.noreply.github.com>
2020-07-07 22:39:55 +0700

    0.1.6
    0.1.6

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -2,14 +2,14 @@ define({
   "name": "API.BKACAD.XYZ",
   "version": "0.1.0",
   "description": "Phiên bản thử nghiệm",
-  "url": "http://localhost/project-2/public/api/",
-  "sampleUrl": "http://localhost/project-2/public/api/",
+  "url": "http://localhost:8080/project-2/public/api/",
+  "sampleUrl": "http://localhost:8080/project-2/public/api/",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-07T07:54:45.328Z",
+    "time": "2020-07-07T15:38:48.545Z",
     "url": "http://apidocjs.com",
-    "version": "0.22.1"
+    "version": "0.23.0"
   }
 });
