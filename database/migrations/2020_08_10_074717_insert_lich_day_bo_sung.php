c5ade687053cf6e07d0c2c46476f3145ebb9904e
anhtrai <34875016+anhtrai@users.noreply.github.com>
2020-08-24 18:27:01 +0700

    0.2.6
    0.2.6

--- database/migrations/2020_08_10_074717_insert_lich_day_bo_sung.php
+++ database/migrations/2020_08_10_074717_insert_lich_day_bo_sung.php
@@ -13,8 +13,11 @@ class InsertLichDayBoSung extends Migration {
         (`ma_lich_day_bo_sung`, `ngay`, `ma_lop`, `ma_nguoi_dung`, `ma_mon_hoc`, `ma_ca`, `ma_phong`, `ghi_chu`, `tinh_trang`) VALUES
         (1, '2020-08-11', 'BKD01K10', 22, 'BKA_ESE', 3, 2, 'sdvasdvs', 1),
         (2, '2020-08-15', 'BIT01K10', 6, 'BIT_ECOM1', 2, 5, '', 1),
-        (4, '2020-08-15', 'BIT01K10', 38, 'BIT_ACC', 3, 3, '', 1),
-        (5, '2020-08-15', 'BIT01K10', 15, 'BIT_ACC', 6, 5, '', 1)"
+        (3, '2020-08-15', 'BIT01K10', 38, 'BIT_ACC', 3, 3, '', 1),
+        (4, '2020-08-15', 'BIT01K10', 15, 'BIT_ACC', 6, 5, '', 1),
+        (5, '2020-08-29', 'BIT01K10', 6, 'BIT_ECOM1', 2, 5, '', 1),
+        (6, '2020-08-29', 'BIT01K10', 38, 'BIT_ACC', 5, 3, '', 1),
+        (7, '2020-08-29', 'BIT01K10', 15, 'BIT_ACC', 7, 5, '', 1)"
         ));
     }
 
